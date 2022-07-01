688b7a34ed62aa0825bfc7d66bdf684644a4fa7c
yxddj1022 <yxddj1022@gmail.com>
2022-07-01 11:10:55 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 new Vue({
   router,
