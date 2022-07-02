e221a41667ea7fdab797ff370b7516e5887bbbbb
yxddj1022 <yxddj1022@gmail.com>
2022-07-02 14:53:05 +0800

    功能图标

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,18 +5,22 @@
     <home-header></home-header>
     <!-- 轮播图 -->
     <home-swiper></home-swiper>
+    <!-- 功能 -->
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
+import HomeIcons from './components/Icons.vue'
 
 export default {
   name: 'home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
---
+++ src/views/home/components/Icons.vue
@@ -0,0 +1,126 @@
+/* 功能图标 */
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img :src="item.imgUrl" alt="">
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-icons',
+  data () {
+    return {
+      iconList: [
+        {
+          id: '00001',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/hotel.png',
+          desc: '酒店'
+        },
+        {
+          id: '00002',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/flight.png',
+          desc: '机票'
+        },
+        {
+          id: '00003',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/train.png',
+          desc: '火车票'
+        },
+        {
+          id: '00004',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/package.png',
+          desc: '度假'
+        },
+        {
+          id: '00005',
+          imgUrl: 'https://s.qunarzz.com/homenode/images/touchheader/piao.png',
+          desc: '景点门票'
+        },
+        {
+          id: '00006',
+          imgUrl: 'https://picbed.qunarzz.com/f5e5770393d759578962e53ee67798c8.png',
+          desc: '海外酒店'
+        },
+        {
+          id: '00007',
+          imgUrl: 'https://picbed.qunarzz.com/a36d2288f19e54562338f4d8ef986288.png',
+          desc: '低价机票'
+        },
+        {
+          id: '00008',
+          imgUrl: 'https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png',
+          desc: '自由行'
+        },
+        {
+          id: '00009',
+          imgUrl: 'https://picbed.qunarzz.com/ae617a31e0bd5803d76918b817f6d942.png',
+          desc: '自由行'
+        }
+      ]
+    }
+  },
+  computed: {
+    // 对iconList做处理，改造成二维数组结构，方便分页展示
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles@/varibles.styl'
+@import '~styles@/mixins.styl'
+
+  .icons >>> .swiper-container
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+  .icons
+    .icon
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      float: left
+      position: relative
+      overflow: hidden
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        padding: .1rem
+        box-sizing: border-box
+        img
+          height: 100%
+          display: block
+          margin: 0 auto
+      .icon-desc
+        height: .44rem
+        line-height: .44rem
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>
